a547f6255665179c605ef664bbb78c44fd044912
mohamedWF67 <mdxx421@gmail.com>
2025-04-30 19:39:40 +0300

    Initial commit

---
+++ src/main/java/com/mycompany/bankaccountmanagmentsystem/BankAccountManagmentSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bankaccountmanagmentsystem;
+
+/**
+ *
+ * @author mohamed waleed
+ */
+public class BankAccountManagmentSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
